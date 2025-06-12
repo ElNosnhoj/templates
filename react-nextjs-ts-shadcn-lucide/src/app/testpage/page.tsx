4ff4ea6b79dc4a24aa74a2ed11ef0923b76ba841
Johnson <j.n.le@outlook.com>
2025-06-12 14:08:48 -0700

    setup

---
+++ react-nextjs-ts-shadcn-lucide/src/app/testpage/page.tsx
@@ -0,0 +1,8 @@
+// http://localhost:3000/testpage
+export default function TestPage() {
+    return (
+        <main className="font-[family-name:var(--font-geist-sans)]">
+            TESTPAGE
+        </main>
+    )
+}
